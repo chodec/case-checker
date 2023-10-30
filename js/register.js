e8ba45753856fca1c5abb0f4d5a0d90fe81a990f
chodec <dom.lepizmo@email.cz>
2023-10-30 19:36:47 +0100

    Add rest of form

--- js/register.js
+++ js/register.js
@@ -1,2 +1,4 @@
 let email = document.getElementById('email')
 let password = document.getElementById('password')
+let nickname = document.getElementById('nickname')
+let passwordRepeat = document.getElementById('passwordRepeat')
