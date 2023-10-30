ee2c017c5c6f335e5f8442cbb589355daaa6657b
chodec <dom.lepizmo@email.cz>
2023-10-30 20:09:36 +0100

    Fix form ui

--- js/register.js
+++ js/register.js
@@ -2,3 +2,9 @@ let email = document.getElementById('email')
 let password = document.getElementById('password')
 let nickname = document.getElementById('nickname')
 let passwordRepeat = document.getElementById('passwordRepeat')
+
+const nicknameHandler = function(e) {
+    console.log(nickname.value);
+}
+
+nickname.addEventListener('keydown', nicknameHandler)
