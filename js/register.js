cff388c67e5644017e29e08ad5c0213b5f3ae8bc
chodec <dom.lepizmo@email.cz>
2023-10-31 19:18:52 +0100

    Email regex

--- js/register.js
+++ js/register.js
@@ -3,13 +3,30 @@ let password = document.getElementById('password')
 let nickname = document.getElementById('nickname')
 let passwordRepeat = document.getElementById('passwordRepeat')
 
+const changeBorder = function (dom, borderColor, boxShadow) {
+    dom.style.borderColor = borderColor
+    dom.style.boxShadow = boxShadow
+    dom.style.color = "white"
+}
+
 const nicknameHandler = function(e) {
     let nicknameLength = nickname.value
     
-    if (nicknameLength.length > 3) {
-        nickname.style.borderColor = "green"
-        nickname.style.boxShadow = "3px 3px green"
+    if (nicknameLength.length > 2) {
+        changeBorder(nickname, "green", "3px 3px green")
+    } else {
+        changeBorder(nickname, "red", "3px 3px red")
+    }
+}
+const emailHandler = function(e) {
+    let emailpattern = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/g
+    
+    if (email.value.match(emailpattern)) {
+        changeBorder(email, "green", "3px 3px green")
+    } else {
+        changeBorder(email, "red", "3px 3px red")
     }
 }
 
 nickname.addEventListener('keydown', nicknameHandler)
+email.addEventListener('keydown', emailHandler)
