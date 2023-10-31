8184bb12e502dcad968e7412c034aba207321f1c
chodec <dom.lepizmo@email.cz>
2023-10-31 19:31:17 +0100

    Fix the form

--- js/register.js
+++ js/register.js
@@ -2,11 +2,15 @@ let email = document.getElementById('email')
 let password = document.getElementById('password')
 let nickname = document.getElementById('nickname')
 let passwordRepeat = document.getElementById('passwordRepeat')
+let emailHelp = document.getElementById('emailHelp')
+let passwordHelp = document.getElementById('passwordHelp')
+let nicknameHelp = document.getElementById('nicknameHelp')
+let passwordRepeatHelp = document.getElementById('passwordRepeatHelp')
 
 const changeBorder = function (dom, borderColor, boxShadow) {
     dom.style.borderColor = borderColor
     dom.style.boxShadow = boxShadow
-    dom.style.color = "white"
+    dom.style.color = "white" 
 }
 
 const nicknameHandler = function(e) {
@@ -14,8 +18,10 @@ const nicknameHandler = function(e) {
     
     if (nicknameLength.length > 2) {
         changeBorder(nickname, "green", "3px 3px green")
+        nicknameHelp.classList.add('hidden')
     } else {
         changeBorder(nickname, "red", "3px 3px red")
+        nicknameHelp.classList.remove('hidden')
     }
 }
 const emailHandler = function(e) {
@@ -23,10 +29,12 @@ const emailHandler = function(e) {
     
     if (email.value.match(emailpattern)) {
         changeBorder(email, "green", "3px 3px green")
+        emailHelp.classList.add('hidden')
     } else {
         changeBorder(email, "red", "3px 3px red")
+        emailHelp.classList.remove('hidden')
     }
 }
 
-nickname.addEventListener('keydown', nicknameHandler)
-email.addEventListener('keydown', emailHandler)
+nickname.addEventListener('change', nicknameHandler)
+email.addEventListener('change', emailHandler)
