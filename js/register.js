6bb62912970fe26420af4d1f8595c3943ee81d99
chodec <dom.lepizmo@email.cz>
2023-10-31 19:43:17 +0100

    Validate password

--- js/register.js
+++ js/register.js
@@ -13,7 +13,7 @@ const changeBorder = function (dom, borderColor, boxShadow) {
     dom.style.color = "white" 
 }
 
-const nicknameHandler = function(e) {
+const nicknameHandler = function() {
     let nicknameLength = nickname.value
     
     if (nicknameLength.length > 2) {
@@ -24,10 +24,10 @@ const nicknameHandler = function(e) {
         nicknameHelp.classList.remove('hidden')
     }
 }
-const emailHandler = function(e) {
-    let emailpattern = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/g
+const emailHandler = function() {
+    let emailPattern = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/g
     
-    if (email.value.match(emailpattern)) {
+    if (email.value.match(emailPattern)) {
         changeBorder(email, "green", "3px 3px green")
         emailHelp.classList.add('hidden')
     } else {
@@ -36,5 +36,22 @@ const emailHandler = function(e) {
     }
 }
 
+
+const passwordHandler = function() {
+    let passwordPattern = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{4,}$/gm
+    console.log(password.length);
+    if (password.value.match(passwordPattern)) {
+        changeBorder(password, "green", "3px 3px green")
+        passwordHelp.classList.add('hidden')
+    } else {
+        changeBorder(password, "red", "3px 3px red")
+        passwordHelp.classList.remove('hidden')
+    }
+}
+
 nickname.addEventListener('change', nicknameHandler)
+nickname.addEventListener('keydown', nicknameHandler)
 email.addEventListener('change', emailHandler)
+email.addEventListener('keydown', emailHandler)
+password.addEventListener('change', passwordHandler)
+password.addEventListener('keydown', passwordHandler)
