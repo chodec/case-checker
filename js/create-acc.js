f387ba9a9e8d3989928caaeeec82e9723e029660
chodec <dom.lepizmo@email.cz>
2023-12-13 20:47:32 +0100

    Add database with user insertion

--- js/create-acc.js
+++ js/create-acc.js
@@ -2,12 +2,46 @@ const express = require('express')
 const bcrypt = require('bcrypt')
 const bodyParser = require("body-parser")
 
+const { Client } = require('pg')
+const client = new Client({
+  user: 'postgres',
+  host: 'localhost',
+  database: 'users',
+  password: '',
+  port: 5432,
+})
+
 const app = express()
 const port = 3000
 
 const saltRounds = 10
 let passHash
 let emailHash
+
+
+
+const insertUser = async (username, email, pass) => {
+  try {
+      await client.connect()
+      await client.query(
+          `INSERT INTO users (username, email, pass) 
+           VALUES ($1, $2, $3)`, [username, email, pass])
+      return true
+  } catch (error) {
+      console.error(error.stack)
+      return false;
+  } finally {
+      await client.end()
+  }
+}
+
+const hashData = (str) => {
+  bcrypt.genSalt(saltRounds, (err, salt ) => {
+    bcrypt.hash(str, salt, (err, hash) => {
+  
+    })
+  })
+}
 
 app.use(bodyParser.urlencoded({extended: true}))
 
@@ -19,13 +53,13 @@ app.use(bodyParser.json());
 
 app.post('/', function(req, res){
     const data = req.body
-    passHash = data.password
-    bcrypt.genSalt(saltRounds, (err, salt ) => {
-      bcrypt.hash(passHash, salt, (err, hash) => {
-        console.log(err)
-        console.log(hash)
-      })
-    })
+    passHash = hashData(data.password)
+    emailHash = hashData(data.email)
+    insertUser(data.nickname, emailHash, passHash).then(result => {
+      if (result) {
+          console.log(result);
+      }
+  });
  })
 
 app.listen(port, () => {
