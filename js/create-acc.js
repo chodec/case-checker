69e2f38349b3ba1253039ad8e98b97d6dfb2c5ed
chodec <dom.lepizmo@email.cz>
2023-12-14 17:51:41 +0100

    check if acc already exists

--- js/create-acc.js
+++ js/create-acc.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const bcrypt = require('bcrypt')
 const bodyParser = require("body-parser")
-const { Client } = require('pg')
+const { Client, Pool } = require('pg')
 const { v4: uuidv4,} = require('uuid');
 
 const client = new Client({
@@ -9,8 +9,10 @@ const client = new Client({
   host: 'localhost',
   database: 'users',
   password: 'F.aq9173',
-  port: 5432,
+  port: '5432'
 })
+const pool = new Pool()
+let response
 
 const app = express()
 const port = 3000
@@ -22,16 +24,30 @@ let id = ''
 
 const insertUser = async (username, email, pass, id) => {
   try {
-      await client.connect()
+      await pool.connect()
       await client.query(
           `INSERT INTO users (username, email, pass, id) 
            VALUES ($1, $2, $3, $4)`, [username, email, pass, id])
       return true
   } catch (error) {
       console.error(error.stack)
-      return false;
+      return false
   } finally {
       await client.end()
+  }
+}
+
+const validateDuplicate = async (email) => {
+  try {
+      await client.connect()
+      response = await client.query(`SELECT email FROM users WHERE email = $1`, [email])
+      return true
+  } catch (error) {
+      console.error(error.stack)
+      return false
+  } finally {
+      await client.end()
+      response.rows[0].email === undefined ? response = true : response = false
   }
 }
 
@@ -41,15 +57,22 @@ app.use(bodyParser.json());
 
 app.post('/', function(req, res){
     const data = req.body
-    id = uuidv4()
-    bcrypt.genSalt(saltRounds, (err, salt ) => {
-      bcrypt.hash(data.password, salt, (err, hash) => {
-        insertUser(data.nickname, data.email, hash, id).then(result => {
-          if (result) {
-              console.log(`User ${data.nickname} created. ID: ${id} Email: ${data.email} Pass: ${hash}`);
-          }
-      });
-      })
+    validateDuplicate(data.email).then(result => {
+      if (response === true) {
+        console.log('Acc already exists.')
+      } else {
+        console.log('Acc does not exist.')
+        // id = uuidv4()
+        // bcrypt.genSalt(saltRounds, (err, salt ) => {
+        //   bcrypt.hash(data.password, salt, (err, hash) => {
+        //     insertUser(data.nickname, data.email, hash, id).then(result => {
+        //       if (result) {
+        //           console.log(`User ${data.nickname} created. ID: ${id} Email: ${data.email} Pass: ${hash}`)
+        //       }
+        //   });
+        //   })
+        // })
+      }
     })
  })
 
