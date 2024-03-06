acffd796622cfb73f801816663077f34ea6e46d6
chodec <dom.lepizmo@email.cz>
2024-03-06 20:06:51 +0100

    refactor queries

--- src/account.js
+++ src/account.js
@@ -1,13 +1,11 @@
 const express = require('express')
 const bcrypt = require('bcrypt')
-const bodyParser = require('body-parser')
-const { Client } = require('pg')
 const { v4: uuidv4} = require('uuid')
 const session = require('express-session')
 const path = require('path')
+const { insertUser, getUser, validateDuplicate, saveSess } = require('./query.js')
 require('dotenv').config()
 
-let queryDuplicate
 let duplicate = true
 let userData = []
 
@@ -18,8 +16,8 @@ const saltRounds = 10
 
 let fePath = path.join(__dirname, '../')
 
-app.use(bodyParser.urlencoded({extended: true}))
-app.use(bodyParser.json())
+app.use(express.urlencoded({extended: true}))
+app.use(express.json())
 
 app.use(express.static(fePath + '/public'))
 
@@ -48,109 +46,15 @@ const requireAuth = (req, res, next) => {
   }
 }
 
-//save session to the DB
-const saveSess = async (sid, sess, expire) => {
-  const client = new Client({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'users',
-    password: process.env.DB_PASS,
-    port: '5432'
-  })
-  try {
-    await client.connect()
-    await client.query(
-        `INSERT INTO user_sessions (sid, sess, expire) 
-         VALUES ($1, $2, $3)`, [sid, sess, expire])
-    return true
-} catch (error) {
-    console.error(error.stack)
-    return false
-} finally {
-    await client.end()
-}
-}
-
-//Create user
-const insertUser = async (username, email, pass, id) => {
-  const client = new Client({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'users',
-    password: process.env.DB_PASS,
-    port: '5432'
-  })
-  try {
-      await client.connect()
-      await client.query(
-          `INSERT INTO users (username, email, pass, id) 
-           VALUES ($1, $2, $3, $4)`, [username, email, pass, id])
-      return true
-  } catch (error) {
-      console.error(error.stack)
-      return false
-  } finally {
-      await client.end()
-  }
-}
-
-//Get users email from DB
-const getUser = async (email) => {
-  const client = new Client({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'users',
-    password: process.env.DB_PASS,
-    port: '5432'
-  })
-  try {
-      await client.connect()
-      userData = await client.query(
-          `SELECT email, pass, id FROM users 
-            WHERE email=($1)`, [email])
-      return true
-  } catch (error) {
-      console.error(error.stack)
-      return false
-  } finally {
-      await client.end()
-  }
-}
-
-//Check if users email already exists in DB
-const validateDuplicate = async (email) => {
-  const client = new Client({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'users',
-    password: process.env.DB_PASS,
-    port: '5432'
-  })
-  try {
-      await client.connect()
-      queryDuplicate = await client.query(`SELECT email FROM users WHERE email = $1`, [email])
-      await client.end()
-      if (queryDuplicate.rowCount >= 1) {
-        duplicate = true
-      } else if (queryDuplicate.rowCount == 0) {
-        duplicate = false
-      }
-      return true
-  } catch (error) {
-      console.error(error.stack)
-      return false
-  } finally {
-      await client.end()
-  }
-}
-
 //Validate users email, if duplicate send 400 error to user
 app.post('/account/register/validateDuplicate', (req, res) => {
   const data = req.body
   validateDuplicate(data.email).then(result => {
-    if (duplicate === false) {
+    if (result === false) {
+      duplicate = false
       res.json(200)
     } else {
+      duplicate = false
       res.json(400)
     }
   })
@@ -179,6 +83,7 @@ app.post('/account/register', (req, res) => {
  app.post('/account/login', (req, res) =>{
   const data = req.body
   getUser(data.email, data.password).then(result => {
+    let userData = result
     bcrypt.compare(data.password, userData.rows[0].pass, (err, result) => {
       if(result){
         req.session.regenerate(function (err) {
---
+++ src/query.js
@@ -0,0 +1,107 @@
+const { Client } = require("pg")
+
+//Create user
+const insertUser = async (username, email, pass, id) => {
+  const client = new Client({
+    user: "postgres",
+    host: "localhost",
+    database: "users",
+    password: process.env.DB_PASS,
+    port: "5432",
+  });
+  try {
+    await client.connect();
+    await client.query(
+      `INSERT INTO users (username, email, pass, id) 
+             VALUES ($1, $2, $3, $4)`,
+      [username, email, pass, id]
+    );
+    return true;
+  } catch (error) {
+    console.error(error.stack)
+    return false
+  } finally {
+    await client.end()
+  }
+};
+
+//save session to the DB
+const saveSess = async (sid, sess, expire) => {
+  const client = new Client({
+    user: "postgres",
+    host: "localhost",
+    database: "users",
+    password: process.env.DB_PASS,
+    port: "5432",
+  });
+  try {
+    await client.connect()
+    await client.query(
+      `INSERT INTO user_sessions (sid, sess, expire) 
+           VALUES ($1, $2, $3)`,
+      [sid, sess, expire]
+    );
+    return true
+  } catch (error) {
+    console.error(error.stack)
+    return false
+  } finally {
+    await client.end()
+  }
+};
+
+//Get users email from DB
+const getUser = async (email) => {
+  const client = new Client({
+    user: "postgres",
+    host: "localhost",
+    database: "users",
+    password: process.env.DB_PASS,
+    port: "5432",
+  })
+  try {
+    await client.connect()
+    userData = await client.query(
+      `SELECT email, pass, id FROM users 
+              WHERE email=($1)`,
+      [email]
+    )
+    return userData
+  } catch (error) {
+    console.error(error.stack)
+    return false
+  } finally {
+    await client.end()
+  }
+};
+
+//Check if users email already exists in DB
+const validateDuplicate = async (email) => {
+  const client = new Client({
+    user: "postgres",
+    host: "localhost",
+    database: "users",
+    password: process.env.DB_PASS,
+    port: "5432",
+  });
+  try {
+    await client.connect()
+    queryDuplicate = await client.query(
+      `SELECT email FROM users WHERE email = $1`,
+      [email]
+    );
+    await client.end()
+    if (queryDuplicate.rowCount >= 1) {
+      return true
+    } else if (queryDuplicate.rowCount == 0) {
+      return false
+    }
+  } catch (error) {
+    console.error(error.stack)
+    return false
+  } finally {
+    await client.end()
+  }
+}
+
+module.exports = { insertUser, getUser, validateDuplicate, saveSess };
