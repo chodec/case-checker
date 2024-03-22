9c833c27323b5bbaf719601c4bc6fef488309d09
chodec <dom.lepizmo@email.cz>
2024-03-22 13:45:46 +0100

    Fix transition

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -3,9 +3,11 @@ const sidebarCollapse = document.getElementById('sidebarCollapse')
 const sidebarCollapseChild = sidebarCollapse.querySelector('i')
 const main = document.getElementById('main')
 const hiddenSmall = document.getElementsByClassName('hiddenSmall')
-let toggled = true
+
+let toggle = true
+
 const toggleNav = () => {
-    if (toggled) {
+    if (toggle) {
         sidebar.style.width = '40px'
         for (let i = 0; i < hiddenSmall.length; i++) {
             hiddenSmall[i].style.display = 'none' 
@@ -13,8 +15,8 @@ const toggleNav = () => {
         sidebarCollapse.style.marginLeft = '20px'
         sidebarCollapseChild.classList.remove = 'fa-chevron-left'
         sidebarCollapseChild.className = 'fa-solid  fa-chevron-right'
-        toggled = false
-    } else if(!toggled){
+        toggle = false
+    } else if(!toggle){
         sidebar.style.width = '250px'
         for (let i = 0; i < hiddenSmall.length; i++) {
             hiddenSmall[i].style.display = 'inline'
@@ -22,6 +24,6 @@ const toggleNav = () => {
         sidebarCollapse.style.marginLeft = '230px'
         sidebarCollapseChild.classList.remove = 'fa-chevron-right'
         sidebarCollapseChild.className = 'fa-solid  fa-chevron-left'
-        toggled = true
+        toggle = true
     }
 }
