af445e3811a46f3200688c616a24a902c7bb580e
chodec <dom.lepizmo@email.cz>
2024-03-22 11:51:20 +0100

    scalable sidebar

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,8 +1,27 @@
 const sidebar = document.getElementById('sidebar')
+const sidebarCollapse = document.getElementById('sidebarCollapse')
+const sidebarCollapseChild = sidebarCollapse.querySelector('i')
 const main = document.getElementById('main')
+const hiddenSmall = document.getElementsByClassName('hiddenSmall')
 let toggled = true
 const toggleNav = () => {
     if (toggled) {
-        sidebar.style.width = '10px'
+        sidebar.style.width = '40px'
+        for (let i = 0; i < hiddenSmall.length; i++) {
+            hiddenSmall[i].style.display = 'none' 
+        }
+        sidebarCollapse.style.marginLeft = '20px'
+        sidebarCollapseChild.classList.remove = 'fa-chevron-left'
+        sidebarCollapseChild.className = 'fa-solid  fa-chevron-right'
+        toggled = false
+    } else if(!toggled){
+        sidebar.style.width = '250px'
+        for (let i = 0; i < hiddenSmall.length; i++) {
+            hiddenSmall[i].style.display = 'inline'
+        }
+        sidebarCollapse.style.marginLeft = '230px'
+        sidebarCollapseChild.classList.remove = 'fa-chevron-right'
+        sidebarCollapseChild.className = 'fa-solid  fa-chevron-left'
+        toggled = true
     }
 }
