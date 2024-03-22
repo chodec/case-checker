6b144822dafc19587ab361414c84e93f1a808b01
chodec <dom.lepizmo@email.cz>
2024-03-22 15:29:02 +0100

    Prepare for data scraping

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -27,3 +27,12 @@ const toggleNav = () => {
         toggle = true
     }
 }
+
+//Vyvoj ceny na marketu konkretniho itemu
+//https://steamcommunity.com/market/pricehistory/?country=CZ&currency=3&appid=730&market_hash_name=P250%20%7C%20Cartel%20%28Battle-Scarred%29
+
+//Konkretni item na steam marketu
+//https://steamcommunity.com/market/priceoverview/?currency=1&appid=730&market_hash_name=P250%20%7C%20Cartel%20%28Battle-Scarred%29
+
+//Might be helpful
+//https://www.npmjs.com/package/steam-market-fetcher
