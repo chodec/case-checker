a0b8a60fc60662dc3981fdc21fcde1d23ec3bf35
chodec <dom.lepizmo@email.cz>
2024-04-04 17:59:32 +0200

    get json file and read it

--- src/middlewares/steamApi.js
+++ src/middlewares/steamApi.js
@@ -1,29 +1,16 @@
-const axios = require('axios')
+const path = require('path')
+const fs = require('fs')
 
-const cases = ['Kilowatt%20Case', 'Dreams%20%26%20Nightmares%20Case', 'Fracture%20Case', 'Recoil%20Case', 'Revolution%20Case', 'Chroma%20Case', 'Chroma%202%20Case', 
-                'Chroma%203%20Case', 'Clutch%20Case', 'CS%3AGO%20Weapon%20Case', 'CS%3AGO%20Weapon%20Case%202', 'CS%3AGO%20Weapon%20Case%203', 'CS20%20Case',
-                'Danger%20Zone%20Case', 'eSports%202013%20Case', 'eSports%202013%20Winter%20Case', 'eSports%202014%20Summer%20Case', 'Falchion%20Case',
-                'Gamma%20Case', 'Gamma%202%20Case', 'Glove%20Case', 'Horizon%20Case', 'Huntsman%20Weapon%20Case', 'Operation%20Bravo%20Case', 'Operation%20Breakout%20Weapon%20Case',
-                'Operation%20Broken%20Fang%20Case', 'Operation%20Hydra%20Case', 'Operation%20Phoenix%20Weapon%20Case', 'Operation%20Riptide%20Case', 'Operation%20Vanguard%20Weapon%20Case',
-                'Operation%20Wildfire%20Case', 'Prisma%20Case', 'Prisma%202%20Case', 'Revolver%20Case', 'Shadow%20Case', 'Shattered%20Web%20Case', 'Snakebite%20Case', 'Spectrum%20Case',
-                'Spectrum%202%20Case', 'Winter%20Offensive%20Weapon%20Case']
-const getCase = async () => {
-    try {
-        const { country, currency, appid, market_hash_name } = req.query;
-        const apiUrl = `https://steamcommunity.com/market/pricehistory/?country=${country}&currency=${currency}&appid=${appid}&market_hash_name=${encodeURIComponent(market_hash_name)}`;
-        
-        const response = await axios.get(apiUrl);
-    
-        if (response.status === 200) {
-            res.json(response.data);
-        } else {
-            console.error('Error fetching price history:', response.statusText);
-            res.status(response.status).json({ error: response.statusText });
-        }
-    } catch (error) {
-        console.error('Error fetching price history:', error);
-        res.status(500).json({ error: 'Failed to fetch price history' });
-    }
-}
+const jsonPath = path.join(__dirname, '../')
+var obj
 
-module.exports = getCase
+// fs.open(jsonPath + '/db/json/Chroma%20Case.json', 'r', (err, data) =>{
+//     if (err) console.log(err)
+
+// })
+
+fs.readFile(jsonPath + '/db/json/Chroma%20Case.json', 'utf8', (err, data) => {
+  if (err) throw err
+  obj = JSON.parse(data)
+  console.log(obj)
+})
--- src/server.js
+++ src/server.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const path = require('path')
 const cors = require('cors')
-const axios = require('axios')
 
 const sessionsMiddleware = require('./middlewares/sessions.js')
 //const getCase = require('./middlewares/steamApi.js')
@@ -12,7 +11,6 @@ const loginRouter = require('./routes/login.js')
 
 const app = express()
 const port = 3000
-const url = 'https://steamcommunity.com/market/pricehistory/?country=cz&currency=1&appid=730&market_hash_name=Kilowatt%20Case'
 
 let fePath = path.join(__dirname, '../')
 
@@ -52,25 +50,6 @@ app.get('/register', (req, res) => {
 
 app.get('/dashboard', (req, res) => {
   res.sendFile(path.join(fePath,'/public/html/dashboard.html'))
-})
-
-app.get('/price-history', async (req, res) => {
-  try {
-    const { country, currency, appid, market_hash_name } = req.query;
-    const apiUrl = `https://steamcommunity.com/market/pricehistory/?country=${country}&currency=${currency}&appid=${appid}&market_hash_name=${encodeURIComponent(market_hash_name)}`;
-    
-    const response = await axios.get(apiUrl);
-
-    if (response.status === 200) {
-        res.json(response.data);
-    } else {
-        console.error('Error fetching price history:', response.statusText);
-        res.status(response.status).json({ error: response.statusText });
-    }
-} catch (error) {
-    console.error('Error fetching price history:', error);
-    res.status(500).json({ error: 'Failed to fetch price history' });
-}
 })
 
 app.listen(port, () => {
