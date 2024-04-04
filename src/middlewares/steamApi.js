58f815b9631a0fc53932c19dbd45367e3b8893d1
chodec <dom.lepizmo@email.cz>
2024-04-04 18:21:40 +0200

    get all cases and get specific case

--- src/middlewares/steamApi.js
+++ src/middlewares/steamApi.js
@@ -2,15 +2,20 @@ const path = require('path')
 const fs = require('fs')
 
 const jsonPath = path.join(__dirname, '../')
-var obj
+let caseName = 'Prisma%202%20Case.json'
 
-// fs.open(jsonPath + '/db/json/Chroma%20Case.json', 'r', (err, data) =>{
-//     if (err) console.log(err)
+const getAllCases = () => {
+    fs.readdir(`${jsonPath}/db/json/`, (err, files) => {
+        files.forEach(file => {
+            console.log(file)
+        })
+    })    
+}
 
-// })
-
-fs.readFile(jsonPath + '/db/json/Chroma%20Case.json', 'utf8', (err, data) => {
-  if (err) throw err
-  obj = JSON.parse(data)
-  console.log(obj)
-})
+const getCase = (caseName) => {
+    fs.readFile(`${jsonPath}/db/json/${caseName}`, 'utf8', (err, data) => {
+        if (err) throw err
+        obj = JSON.parse(data)
+        console.log(data)
+    })  
+}
