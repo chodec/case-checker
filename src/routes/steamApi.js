2e9ddefd5c22c15b294bbac142dcf2bff000335b
chodec <dom.lepizmo@email.cz>
2024-05-04 00:01:35 +0200

    get current price

--- src/routes/steamApi.js
+++ src/routes/steamApi.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const fs = require('fs')
 const router = express.Router()
+const axios = require('axios')
+
 
 //https://stackoverflow.com/questions/29902280/get-price-of-item-in-steam-community-market-with-json
 //get current item value
@@ -16,6 +18,18 @@ router.get('/pricehistory/case/:filename', (req, res) => {
         } else {
             const jsonData = JSON.parse(data)
             res.json(jsonData)
+        }
+    })
+})
+
+router.get('/currentprice/case/:filename', (req, res) =>{
+    const filename = req.params.filename
+    const encodedFileName = encodeURIComponent(filename)
+    axios.get(`https://steamcommunity.com/market/priceoverview/?appid=730&market_hash_name=${encodedFileName}&currency=1`).then(function(data) {
+        if (data.status === 200){
+            res.send(data.data)
+        } else {
+            res.status(500).send('Error reading file')
         }
     })
 })
