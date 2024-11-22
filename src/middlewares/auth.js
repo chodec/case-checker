2a3b32019a84354283bd76340c98b0ccfe5c5165
chodec <dom.lepizmo@email.cz>
2024-11-22 11:47:37 +0100

    fix login error

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -3,12 +3,13 @@ const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY
 
 const verifyToken = (req, res, next) => {
   const authHeader = req.headers['authorization']
-  
-  if (!authHeader || !authHeader.startsWith('Bearer ')) {
+  const token = authHeader
+    ? authHeader.split(' ')[1]
+    : req.cookies.token
+
+  if (!token) {
     return res.status(403).json({ message: 'No token provided or invalid format' })
   }
-
-  const token = authHeader.split(' ')[1] // Extract the token after "Bearer "
 
   jwt.verify(token, JWT_SECRET_KEY, (err, decoded) => {
     if (err) {
@@ -16,7 +17,7 @@ const verifyToken = (req, res, next) => {
       return res.status(403).json({ message: 'Invalid token' })
     }
 
-    req.user = decoded // Attach decoded user info if needed
+    req.user = decoded
     next()
   })
 }
--- src/server.js
+++ src/server.js
@@ -1,62 +1,57 @@
 const express = require('express')
 const path = require('path')
 const cors = require('cors')
-const sessionsMiddleware = require('./middlewares/sessions.js')
+const cookieParser = require('cookie-parser')
+const sessionsMiddleware = require('./middlewares/sessions')
 const { verifyToken } = require('./middlewares/auth')
 
-// Importing routes
-const registerRouter = require('./routes/register.js')
-const validateRouter = require('./routes/validate.js')
-const loginRouter = require('./routes/login.js')
-const steamApi = require('./routes/steamApi.js')
-const insertAsset = require('./routes/insertAsset.js')
-const getUserAssets = require('./routes/getUserAssets.js')
-const countUserAssets = require('./routes/countUserAssets.js')
-const deleteUserAsset = require('./routes/deleteUserAsset.js')
-const portfolioPerformance = require('./routes/portfolioPerformance.js')
+const registerRouter = require('./routes/register')
+const validateRouter = require('./routes/validate')
+const loginRouter = require('./routes/login')
+const steamApi = require('./routes/steamApi')
+const insertAsset = require('./routes/insertAsset')
+const getUserAssets = require('./routes/getUserAssets')
+const countUserAssets = require('./routes/countUserAssets')
+const deleteUserAsset = require('./routes/deleteUserAsset')
+const portfolioPerformance = require('./routes/portfolioPerformance')
 
 const app = express()
 const port = 3000
-
 let fePath = path.join(__dirname, '../')
 
-// Middleware setup
 app.use(cors())
+app.use(cookieParser())
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.use(express.static(fePath + '/public'))
-
 app.use(sessionsMiddleware)
 
-// Unprotected routes
 app.use(validateRouter)
 app.use(registerRouter)
 app.use(loginRouter)
 
-// Protected routes with verifyToken middleware
 app.use('/asset', verifyToken, getUserAssets)
 app.use('/asset', verifyToken, countUserAssets)
 app.use('/asset', verifyToken, deleteUserAsset)
 app.use('/asset', verifyToken, portfolioPerformance)
 app.use('/asset', verifyToken, insertAsset)
 
-// Secure the dashboard route with verifyToken
 app.get('/dashboard', verifyToken, (req, res) => {
   res.sendFile(path.join(fePath, '/public/html/dashboard.html'))
 })
 
-// Other routes
 app.get('/', (req, res) => {
   res.sendFile(path.join(fePath, '/public/html/index.html'))
 })
+
 app.get('/login', (req, res) => {
   res.sendFile(path.join(fePath, '/public/html/login.html'))
 })
+
 app.get('/register', (req, res) => {
   res.sendFile(path.join(fePath, '/public/html/register.html'))
 })
 
-// Start the server
 app.listen(port, () => {
   console.log(`App listening on port ${port}`)
 })
